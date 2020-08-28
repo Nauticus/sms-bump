8345b977f98df975f2e71896583b489977f167d7
Emanuil Ganchev <emanuilganchev@Emanuils-MacBook-Pro.local>
2020-08-28 05:36:35 +0300

    chore(config): add initial webpack config;

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// @ts-check
+const path = require('path');
+
+/** @type { import('webpack').Configuration } */
+const config = {
+    entry: path.resolve(__dirname, "src/index.tsx"),
+    mode: process.env.NODE_ENV === "development" ? "development" : "production",
+    output: {
+        filename: "[name].[chunkhash].js",
+        path: path.resolve(__dirname, "build/"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts)?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            app: path.resolve(__dirname, "src/"),
+        },
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: "initial",
+                    name: "vendor",
+                    enforce: true,
+                },
+            },
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "SMS Bump - Forms",
+            template: "!!handlebars-loader!src/index.handlebars",
+        }),
+    ],
+    devtool: "source-map",
+    performance: {
+        hints: process.env.NODE_ENV === "production" ? "warning" : false,
+    },
+};
+
