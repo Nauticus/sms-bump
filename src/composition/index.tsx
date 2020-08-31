4ca116fe2d129c6671f202f5d4c506c4862c3f73
Emanuil Ganchev <emanuilganchev@Emanuils-MacBook-Pro.local>
2020-08-31 23:00:33 +0300

    fix(imports): reorder imports;

--- src/composition/index.tsx
+++ src/composition/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 import theme, { GlobalStyles } from "app/style";
-import Header from "./header";
 import AppContainer from 'app/components/app-container';
 
+import Header from "./header";
 import FormPage from "./form-page";
 
 const App: React.FC<Record<string, unknown>> = () => (
